37e280facee30a80437a68cca490c0680da8ee08
DarqSied <himanshu.sharma27@protonmail.com>
2022-03-28 23:02:18 +0530

    backing up

--- dotfiles/.config/dwm/config.def.h
+++ dotfiles/.config/dwm/config.def.h
@@ -4,14 +4,14 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx       = 2;   /* border pixel of windows */
+static const unsigned int borderpx       = 0;   /* border pixel of windows */
 static const unsigned int snap           = 32;  /* snap pixel */
 static const unsigned int gappih         = 5;  /* horiz inner gap between windows */
 static const unsigned int gappiv         = 5;  /* vert inner gap between windows */
 static const unsigned int gappoh         = 5;  /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 5;  /* vert outer gap between windows and screen edge */
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
-static const int smartgaps_fact          = 0;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int smartgaps_fact          = 1.5;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
@@ -30,14 +30,14 @@ static const char dmenufont[]            = "Hack Nerd Font:size=11";
 
 static char c000000[]                    = "#000000"; // placeholder value
 
-static char normfgcolor[]                = "#ffffff";
+static char normfgcolor[]                = "#f8f8f2";
 static char normbgcolor[]                = "#000000";
-static char normbordercolor[]            = "#44475a";
+static char normbordercolor[]            = "#000000";
 static char normfloatcolor[]             = "#6272a4";
 
 static char selfgcolor[]                 = "#f8f8f2";
-static char selbgcolor[]                 = "#bd93f9";
-static char selbordercolor[]             = "#bd93f9";
+static char selbgcolor[]                 = "#000000";
+static char selbordercolor[]             = "#44475a";
 static char selfloatcolor[]              = "#bd93f9";
 
 static char titlenormfgcolor[]           = "#f8f8f2";
@@ -76,7 +76,7 @@ static const unsigned int borderalpha = 0;
 static const unsigned int alphas[][3] = {
 	/*                       fg      bg        border     */
 	[SchemeNorm]         = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]         = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]          = { OPAQUE, baralpha, borderalpha },
 	[SchemeTitleNorm]    = { OPAQUE, baralpha, borderalpha },
 	[SchemeTitleSel]     = { OPAQUE, baralpha, borderalpha },
 	[SchemeTagsNorm]     = { OPAQUE, baralpha, borderalpha },
@@ -213,7 +213,7 @@ static const BarRule barrules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.5;  /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55;  /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
