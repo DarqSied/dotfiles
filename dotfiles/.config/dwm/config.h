829cbfbccd5d321cf001020ca7389851d7248a30
DarqSied <himanshu.sharma27@protonmail.com>
2022-03-28 23:05:05 +0530

    backing up

--- dotfiles/.config/dwm/config.h
+++ dotfiles/.config/dwm/config.h
@@ -11,7 +11,7 @@ static const unsigned int gappiv         = 5;  /* vert inner gap between windows
 static const unsigned int gappoh         = 5;  /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 5;  /* vert outer gap between windows and screen edge */
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
-static const int smartgaps_fact          = 1.5;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int smartgaps_fact          = 0;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
