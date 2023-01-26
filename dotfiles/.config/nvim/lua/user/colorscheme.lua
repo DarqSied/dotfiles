8f6d662d8397155c536b2198344e527cf86c64e6
DarqSied <himanshu.sharma27@protonmail.com>
2023-01-26 23:38:52 +0530

    Backing up

--- dotfiles/.config/nvim/lua/user/colorscheme.lua
+++ dotfiles/.config/nvim/lua/user/colorscheme.lua
@@ -1,8 +1,6 @@
-local colorscheme = "neon"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
-    return
+  return
 end
-
