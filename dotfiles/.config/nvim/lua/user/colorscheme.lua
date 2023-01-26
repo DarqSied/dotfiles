aaf647fe22dab0b74f97c9b7e284148eb1d4427c
DarqSied <himanshu.sharma27@protonmail.com>
2023-01-27 00:20:56 +0530

    Backing up

--- dotfiles/.config/nvim/lua/user/colorscheme.lua
+++ dotfiles/.config/nvim/lua/user/colorscheme.lua
@@ -1,5 +1,12 @@
 local colorscheme = "tokyonight"
 
+require("tokyonight").setup({
+  style = "storm",
+  light_style = "day",
+  transparent = true,
+  terminal_colors = true,
+})
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
