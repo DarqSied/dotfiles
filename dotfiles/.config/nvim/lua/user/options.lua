e3cf3100afce6d396a5910ef25703d408c0e748b
DarqSied <himanshu.sharma27@protonmail.com>
2023-03-11 13:42:35 +0530

    Backing up

--- dotfiles/.config/nvim/lua/user/options.lua
+++ dotfiles/.config/nvim/lua/user/options.lua
@@ -26,7 +26,7 @@ local options = {
   tabstop = 4,                             -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
+  relativenumber = true,                  -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
 
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
@@ -34,7 +34,7 @@ local options = {
   linebreak = true,                        -- companion to wrap, don't split words
   scrolloff = 10,                           -- minimal number of screen lines to keep above and below the cursor
   sidescrolloff = 5,                       -- minimal number of screen columns either side of cursor if wrap is `false`
-  guifont = "Hack:h18",                    -- the font used in graphical neovim applications
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
